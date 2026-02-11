6422998136d2ac60814f47c5fb6ed7bc13135ed8
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2026-02-11 15:01:45 -0300

    Creating structure
    We started building our initial structure so that in the upcoming commits we can add all the components to the pages.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Link Shortener</title>
+</head>
+
+<body>
+
+
+    <?php
+
+
+    ?>
+
+
+</body>
+
+</html>
