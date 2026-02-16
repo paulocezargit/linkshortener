3d1b3382ce8d84b33eb5e750a17d91925b7f4541
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2026-02-16 12:08:40 -0300

    Creating redirection structures
    This time, we started creating the main structures that we will use to implement and manage page redirection. When a specific value is passed in the URL, the page will be redirected to the address stored in the database.

--- index.php
+++ index.php
@@ -1,21 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+# ================================================================
+# This is the required structure for our system to recognize the 
+# shortened URL, which will be used to query the database to 
+# determine the correct URL for page redirection.
+# ================================================================
+$path_url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$path_url = trim($path_url, '/');
+$pieces_url = explode('/', $path_url);
+$last_value = end($pieces_url);
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Link Shortener</title>
-</head>
+if (!empty($last_value)) {
 
-<body>
+    # ================================================================
+    # Test to verify if our URL reader is working. Soon, it will be 
+    # changed to use a database query structure with SQL.
+    # ================================================================
+    if ($last_value == "A313c2") {
+        header("Location: https://www.google.com");
+        exit;
+    } elseif ($last_value == "b155jk2") {
+        header("Location: https://www.youtube.com");
+        exit;
+    }
+
+} else {
+
+}
 
 
-    <?php
+?>
 
+<?php include("header.php"); ?>
 
-    ?>
+<!--Context-->
 
-
-</body>
-
-</html>
+<?php include("footer.php"); ?>
--- user.php
+++ user.php
@@ -1,5 +1,5 @@
-<?php
+<?php include("header.php"); ?>
 
-//User
+<!--Context-->
 
-?>
+<?php include("footer.php"); ?>
