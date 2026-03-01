f6966f118be1f600da9596729c1f17849c87e7cb
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2026-02-28 21:24:30 -0300

    Creating the Login Page Structure
    This time, we began designing the visual appearance of the login page, where users will access their accounts. This process and its implementation will be straightforward.

--- login.php
+++ login.php
@@ -1,5 +1,21 @@
 <?php include("header.php"); ?>
 
-<!--Context-->
+
+<div class="boxlogin">
+    <form class="boxlogin-container">
+        <div class="boxlogin-title">Link Shortener</div>
+        <div class="boxlogin-group">
+            <label class="boxlogin-label">E-mail</label>
+            <input class="boxlogin-input" type="text" name="login" id="login" placeholder="">
+        </div>
+        <div class="boxlogin-group">
+            <label class="boxlogin-label">Password</label>
+            <input class="boxlogin-input" type="text" name="password" id="password" placeholder="">
+        </div>
+        <div class="boxlogin-center">
+            <button type="sumit" class="boxlogin-submit">Sign in</button>
+        </div>
+    </form>
+</div>
 
 <?php include("footer.php"); ?>
