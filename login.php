e09f758e4f145a55d46a4dbca6f534939cbf967b
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2026-03-06 22:10:52 -0300

    Making some adjustments
    This time we made some corrections to the login page and the CSS stylesheet.

--- login.php
+++ login.php
@@ -6,11 +6,11 @@
         <div class="boxlogin-title">Link Shortener</div>
         <div class="boxlogin-group">
             <label class="boxlogin-label">E-mail</label>
-            <input class="boxlogin-input" type="text" name="login" id="login" placeholder="">
+            <input class="boxlogin-input" type="text" name="login" id="login" placeholder="" autocomplete="off" autocorrect="off" spellcheck="false" maxlength="128" >
         </div>
         <div class="boxlogin-group">
             <label class="boxlogin-label">Password</label>
-            <input class="boxlogin-input" type="text" name="password" id="password" placeholder="">
+            <input class="boxlogin-input" type="password" name="password" id="password" placeholder="" autocomplete="off" autocorrect="off" spellcheck="false" maxlength="30" >
         </div>
         <div class="boxlogin-center">
             <button type="sumit" class="boxlogin-submit">Sign in</button>
